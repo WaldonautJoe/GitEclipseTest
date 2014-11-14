f98222e92045b0c9b1fbc1f24ccd10018730066c
WaldonautJoe <omanacent@gmail.com>
2014-11-14 10:49:46 -0600

    Reconjigger the duck

--- GitEclipseTest/src/jneickhoff/git/second/HelloGit.java
+++ GitEclipseTest/src/jneickhoff/git/second/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Gitting along quite fine.");
+		System.out.println("Whaaat is this line doing here?");
 	}
 
 }
